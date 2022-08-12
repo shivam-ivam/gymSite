df0fb0d819eb8dcec5e0711cecf7e0afc19f145b
shivam ivam <ivamanonymous@gmail.com>
2022-08-12 23:03:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const cross_btn = document.querySelector(".btn-cross");
+const hamberg_btn = document.querySelector(".btn-hamberg");
+const nav = document.querySelector("nav");
+
+
+
+// cross button 
+cross_btn.addEventListener("click", function () {
+    nav.classList.add("hidden");
+    hamberg_btn.classList.remove("hidden");
+    cross_btn.classList.add("hidden");
+});
+
+// hamberg button
+hamberg_btn.addEventListener("click", function () {
+    nav.classList.remove("hidden");
+    hamberg_btn.classList.add("hidden");
+    cross_btn.classList.remove("hidden");
+});
