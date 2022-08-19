dca3f7e0cf78ffe4650a2a6414381792c18d1bc3
shivam ivam <ivamanonymous@gmail.com>
2022-08-19 13:40:19 +0530

    upto sec 3

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const cross_btn = document.querySelector(".btn-cross");
+const cross_btn_modal = document.querySelector(".btn-cross-modal");
 const hamberg_btn = document.querySelector(".btn-hamberg");
 const nav = document.querySelector("nav");
+const equipment_btn = document.querySelector(".sec-1-btn2");
+const equipment_section = document.querySelector(".sec-3");
+
 
 
 
@@ -9,6 +13,7 @@ cross_btn.addEventListener("click", function () {
     nav.classList.add("hidden");
     hamberg_btn.classList.remove("hidden");
     cross_btn.classList.add("hidden");
+
 });
 
 // hamberg button
@@ -17,3 +22,14 @@ hamberg_btn.addEventListener("click", function () {
     hamberg_btn.classList.add("hidden");
     cross_btn.classList.remove("hidden");
 });
+
+// showing equipments section 
+equipment_btn.addEventListener("click", function () {
+    equipment_section.classList.remove("hidden");
+    cross_btn_modal.classList.remove("hidden");
+});
+cross_btn_modal.addEventListener("click", function () {
+    equipment_section.classList.add("hidden");
+    cross_btn_modal.classList.add("hidden");
+});
+
