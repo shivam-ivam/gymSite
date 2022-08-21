fbd324fe4ec91f624a029089a9f92972b3e43111
shivam ivam <ivamanonymous@gmail.com>
2022-08-21 10:26:50 +0530

    contact page added

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ const hamberg_btn = document.querySelector(".btn-hamberg");
 const nav = document.querySelector("nav");
 const equipment_btn = document.querySelector(".sec-1-btn2");
 const equipment_section = document.querySelector(".sec-3");
-
+const contact_page = document.querySelector(".contact-page");
+const contact_btn1 = document.querySelector(".sec-1-btn1")
+const contact_btn2 = document.querySelector(".contact-btn")
+const contact_btn3 = document.querySelector(".nav-btn-3")
 
 
 
@@ -31,5 +34,21 @@ equipment_btn.addEventListener("click", function () {
 cross_btn_modal.addEventListener("click", function () {
     equipment_section.classList.add("hidden");
     cross_btn_modal.classList.add("hidden");
+    contact_page.classList.add("hidden");
+});
+
+
+//  contact btn work 
+contact_btn1.addEventListener("click", function () {
+    cross_btn_modal.classList.remove("hidden");
+    contact_page.classList.remove("hidden");
+});
+contact_btn2.addEventListener("click", function () {
+    cross_btn_modal.classList.remove("hidden");
+    contact_page.classList.remove("hidden");
+});
+contact_btn3.addEventListener("click", function () {
+    cross_btn_modal.classList.remove("hidden");
+    contact_page.classList.remove("hidden");
 });
 
